0d8fae37dce92091905a39dc70ef6eea3f1cce7b
maryursul <ursulmar@gmail.com>
2019-10-18 10:51:45 +0300

    add contact info , add scrol functionality by id , make 'to the top' button

--- src/index.js
+++ src/index.js
@@ -11,3 +11,47 @@ $(window).scroll(() => {
     $('#navbar').animate({opacity: `1`}, 1500)
   }
 });
+
+$(window).scroll(() => {
+ 
+  if($(this).scrollTop() != 0) {
+   
+  $('#toTop').fadeIn();
+   
+  } else {
+   
+  $('#toTop').fadeOut();
+   
+  }
+   
+  });
+   
+  $('#toTop').click(function() {
+   
+  $('body,html').animate({scrollTop:0},800);
+   
+  });
+
+  const navbarScroll = (scrollTop) => {
+    $('html, body').animate({
+      scrollTop: scrollTop
+    }, 1300);
+  }
+  
+  $(document).ready(() => {
+    $(this).scrollTop(0);
+  });
+  
+
+  $('#general-info').click(() => {
+    navbarScroll($('#general').offset().top - 56);
+  });
+  $('#photoes').click(() => {
+    navbarScroll($('#photoes-info').offset().top - 56);
+  });
+  $('#contact-info').click(() => {
+    navbarScroll($('#contact').offset().top - 56);
+  });
+  $('#more-info').click(() => {
+    navbarScroll($('#general').offset().top - 56)
+  })
