44a42842654b618e50a03e0b23ff8f4bfddc17cc
maryursul <ursulmar@gmail.com>
2019-10-27 20:26:45 +0200

    Fix minore bugs, refactore code, made some improvements

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 $(document).ready(() => {
-  $('#main-background').animate({height: `1000px`}, 1700);
-  $('#main-text').animate({opacity: 2}, 100);
+  $('#main-background').animate({height: `1000px`}, 1723);
+  $('#main-text').animate({opacity: 2}, 2323);
 });
-
+//коли заходиш на сайт верхня анімація
 
 $(window).scroll(() => {
-  let scrollTop = $(window).scrollTop();
+  let scrollTop = $(window).scrollTop(); //вираховує к-сть пікселів які ми прокрутили зверху
   
   if (scrollTop > $('#main-background').height()/20) {
     $('#navbar').animate({opacity: `1`}, 1500)
   }
 });
+//коли ми скролимо то появляється навбар
 
 $(window).scroll(() => {
-  console.log('this', this);
-  
  
   if($(this).scrollTop() != 0) {
    
@@ -28,22 +27,24 @@ $(window).scroll(() => {
    
   });
    
-  $('#toTop').click(function() {
+  $('#toTop').click(() => {
    
   $('body,html').animate({scrollTop:0},800);
    
   });
+
+  //логіка кнопки топ
 
   const navbarScroll = (scrollTop) => {
     $('html, body').animate({
       scrollTop: scrollTop
     }, 1300);
   }
-  
-  $(document).ready(() => {
-    $(this).scrollTop(0);
-  });
-  
+  //якщо в навбарі є менюшка, якої немає в html то вертає наверх
+  // $(document).ready(() => {
+  //   $(this).scrollTop(0);
+  // });
+  // //якщо ми вс
 
   $('#general-info').click(() => {
     navbarScroll($('#general').offset().top - 56);
